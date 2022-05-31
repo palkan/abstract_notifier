ce5faf75fb468a8161959fafe097bf7b865ea958
komagata <komagata@gmail.com>
2022-05-31 22:01:36 +0900

    Change assertion name and arguments.

--- lib/abstract_notifier/testing/minitest.rb
+++ lib/abstract_notifier/testing/minitest.rb
@@ -1,15 +1,41 @@
 # frozen_string_literal: true
 
-module Minitest
-  module Assertions
-    def assert_delivery(args)
+module AbstractNotifier
+  module TestHelper
+    def assert_notifications_sent(count, params)
       yield
-      assert_equal args, AbstractNotifier::Testing::Driver.deliveries.last
+      assert_equal deliveries.count, count
+      count.times do |i|
+        delivery = deliveries[0 - i]
+        msg = message(msg) { "Expected #{mu_pp(delivery)} to include #{mu_pp(params)}" }
+        assert hash_include?(delivery, params), msg
+      end
     end
 
-    def assert_async_delivery(args)
+    def assert_notifications_enqueued(count, params)
       yield
-      assert_equal args, AbstractNotifier::Testing::Driver.enqueued_deliveries.last
+      assert_equal enqueued_deliveries.count, count
+      count.times do |i|
+        delivery = enqueued_deliveries[0 - i]
+        msg = message(msg) { "Expected #{mu_pp(delivery)} to include #{mu_pp(params)}" }
+        assert hash_include?(delivery, params), msg
+      end
+    end
+
+    private
+
+    def deliveries
+      AbstractNotifier::Testing::Driver.deliveries
+    end
+
+    def enqueued_deliveries
+      AbstractNotifier::Testing::Driver.enqueued_deliveries
+    end
+
+    def hash_include?(haystack, needle)
+      needle.all? do |k, v|
+        haystack.key?(k) && haystack[k] == v
+      end
     end
   end
 end
